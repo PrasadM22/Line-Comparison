a060de723c2debd3c206ef0b4b71cbab6f5327a2
RagahavM22 <pmurhekar8@gmail.com>
2021-05-09 22:10:00 +0530

    Change Made

--- Line Comparison/src/problem/Cartesian_Coordinates.java
+++ Line Comparison/src/problem/Cartesian_Coordinates.java
@@ -1,8 +1,15 @@
 package problem;
 
 public class Cartesian_Coordinates {
+	        //Declaration
+			public static int x1=4;
+			public static int y1=3;
+			public static int x2=6;
+			public static int y2=8;
 		public static void main(String[] args) {
 			System.out.println("Welcome to Line Comparison Computation ");
-
+			
+			double line_length = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+			System.out.println("length of Line is: "+line_length);
 			}
 	}
