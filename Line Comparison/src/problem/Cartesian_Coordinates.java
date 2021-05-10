841d020d89176d748d933ee7cb996a935da96b1e
RagahavM22 <pmurhekar8@gmail.com>
2021-05-10 16:36:30 +0530

    File Added

--- Line Comparison/src/problem/Cartesian_Coordinates.java
+++ Line Comparison/src/problem/Cartesian_Coordinates.java
@@ -1,36 +1,25 @@
 package problem;
 
 public class Cartesian_Coordinates {
-	        //Declaration
-			static int x1;
-			static int y1;
-			static int x2;
-			static int y2;
-			//Assign Value to Class Member
-			 public static void Assign_value(int x1,int y1,int x2,int y2) {
-				Cartesian_Coordinates.x1 = x1;
-				Cartesian_Coordinates.y1 = y1;
-				Cartesian_Coordinates.x2 = x2;
-				Cartesian_Coordinates.y2 = y2;
+			//Method to find length of line
+			static double Calculate_length(int x1,int y1,int x2,int y2)
+			{
+				return Math.sqrt((x2-x1)^2 + (y2-y1)^2);
 			}
 			 //Main Method
 			 public static void main(String[] args) 
 			 {
 			System.out.println("Welcome to Line Comparison Computation ");
-			Assign_value(4, 3, 6, 8);
-			double line1_length = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-			System.out.println("length of Line_1 is: "+line1_length);
-			Assign_value(4, 3, 6, 8);
-			double line2_length = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-			System.out.println("length of Line_2 is: "+line2_length);
+			System.out.println("length of Line_1 is: "+Calculate_length(4, 3, 6, 8));
+			System.out.println("length of Line_1 is: "+Calculate_length(3, 4, 6, 9));
 			
 			//Checking Equality of Two Line
-			
-			if(line1_length == line2_length)
-				System.out.println("Both line are Equal");
+			int Checker = Double.compare(Calculate_length(4, 3, 6, 8),Calculate_length(3, 4, 6, 9));
+			if(Checker > 0)
+				System.out.println("line 1 is Greater than line 2");
+			else if(Checker < 0)
+				System.out.println("line 2 is Greater than line 1");
 			else
-				System.out.println("Both line are Not Equal");
-			}
-			
+				System.out.println("Both line are Equal");
 	}
-	
+}
